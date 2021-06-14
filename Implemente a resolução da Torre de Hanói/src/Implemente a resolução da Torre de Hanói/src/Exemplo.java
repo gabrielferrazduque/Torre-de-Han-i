a75963250d9b98d6c0390fcbd5b66aad50ea0bce
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2021-06-14 20:14:01 -0300

    Add files via upload

---
+++ Implemente a resolução da Torre de Hanói/src/Implemente a resolução da Torre de Hanói/src/Exemplo.java
@@ -0,0 +1,30 @@
+}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Exemplo {
+
+  public static void main(String[] args) throws IOException {
+    Scanner ler = new Scanner(System.in);
+    int i, n;
+
+    System.out.printf("Informe o número para a tabuada:\n");
+    n = ler.nextInt();
+
+    FileWriter arq = new FileWriter("d:\\tabuada.txt");
+    PrintWriter gravarArq = new PrintWriter(arq);
+
+    gravarArq.printf("+--Resultado--+%n");
+    for (i=1; i<=10; i++) {
+      gravarArq.printf("| %2d X %d = %2d |%n", i, n, (i*n));
+    }
+    gravarArq.printf("+-------------+%n");
+
+    arq.close();
+
+    System.out.printf("\nTabuada do %d foi gravada com sucesso em \"d:\\tabuada.txt\".\n", n);
+  }
+ 
+}
